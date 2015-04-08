c5a258197cc9c3f0538510ebcc0e8e96efa72071
unknown <micheldc@windowslive.com>
2015-04-08 19:42:55 +0200

    minor

--- workspace/Scriptempus/src/test/Tester.java
+++ workspace/Scriptempus/src/test/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
 
         TimeGenerator tg = new TimeGenerator(0, 0);
 
-        for (int i = 0; i < 24; i++) {
+        for (int i = 0; i < 24; i++) { 
             for (int j = 0; j < 59; j++) {
             	tg = new TimeGenerator(i, j);
                 System.out.println(i + ":" + j + " -> " + tg);
